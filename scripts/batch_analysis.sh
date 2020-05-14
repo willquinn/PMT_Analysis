bc981b7bc74cd62dba85698b3377fb8b94afe7d9
William Quinn <william.quinn.14@ucl.ac.uk>
2020-05-14 14:57:59 +0100

    bug fix

--- scripts/batch_analysis.sh
+++ scripts/batch_analysis.sh
@@ -12,8 +12,6 @@ done
 root_dir="/unix/nemo4/PMT_He_Study_nemo4"
 script_log_dir=batch_log_$tag
 mkdir $root_dir/batch_files/$script_log_dir
-# mkdir $root_dir/batch_files/$script_log_dir/scripts
-mkdir $root_dir/batch_files/$script_log_dir/logs
 
 # Files found  automatically
 ls $root_dir/data/ROOT_files/*.root > $root_dir/batch_files/$script_log_dir/filenames.ascii
@@ -27,7 +25,6 @@ for file in $data_file_list;
 do
   executable_plus_arguments="python3 $source_code -i $file -c $config_file -o /home/wquinn/pmt_analysis/output.txt"
 
-  interactive_command="$executable_plus_arguments >& $root_dir/batch_files/$script_log_dir/logs/batch_file_$ifile.log"
-  echo $interactive_command
+  interactive_command="$executable_plus_arguments &"
   $interactive_command
 done
