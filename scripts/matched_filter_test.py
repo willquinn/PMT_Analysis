18b6023122c6740c440a89fbd3731c795442d0d1
William Quinn <william.quinn.14@ucl.ac.uk>
2020-05-29 10:35:12 +0100

    update

--- scripts/matched_filter_test.py
+++ scripts/matched_filter_test.py
@@ -53,6 +53,8 @@ def main():
     tree = file.T
     tree.Print()
 
+    num_events = 1000
+
     topology = [4, 1]
     pmt_array = PMT_Array(topology, date + "_" + time)
     pmt_array.set_pmt_id("GAO607_" + date + "_" + time, 0)
@@ -76,12 +78,22 @@ def main():
                 "~/Desktop/190621_A1400_B1400_templates.root",
                 ["A1400_B1400_Ch0_Template", "A1400_B1400_Ch1_Template", "A1400_B1400_Ch0_Template", "A1400_B1400_Ch1_Template"])
 
+    amp_cut = pmt_array.get_pmt_object_number(0).get_setting("mf_amp_threshold")
+    shape_cut = pmt_array.get_pmt_object_number(0).get_setting("mf_shape_threshold")
+
     t_injected = [[], []]
     a_injected = [[], []]
+    a_injected_failure = [[], []]
+    t_injected_success = [[], []]
+    t_injected_failure = [[], []]
+    mf_a_injected = [[], []]
+    mf_s_injected = [[], []]
 
     num = [0, 0]
     enum = [0, 0]
     iterator = 0
+
+    ran_range = 52
 
     injected_array = [[[], []], [[], []]]
     best_array = [[[], []], [[], []]]
@@ -91,7 +103,7 @@ def main():
     #bar = progressbar.ProgressBar(maxval=tree.GetEntries(), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
     #bar.start()
 
-    for event in tree:
+    for event in tqdm.tqdm(tree):
         OM_ID = event.OM_ID
         waveform = np.array(event.waveform)
 
@@ -102,7 +114,7 @@ def main():
         temp_amplitude = np.amin(template_pulse)
 
         # Create random numbers
-        random_amp = rand.randrange(0, 101, 2)
+        random_amp = rand.randrange(0, ran_range - 1, 2)
         random_time = rand.randrange(800, pmt_array.get_pmt_object_number(OM_ID).get_waveform_length() - template_pulse.size)
 
         '''print("A: ",random_amp)
@@ -125,6 +137,7 @@ def main():
         '''plt.plot(waveform, 'b-')
         plt.plot(waveform - injected_data, 'r-')
         plt.show()'''
+        check = False
 
         if pmt_waveform.get_pmt_pulse_trigger():
             i_shape = 0
@@ -132,20 +145,26 @@ def main():
             r_shape = 0
             r_amp = 0
 
-            print(random_time, pmt_waveform_injected.get_pmt_pulse_times())
-
             for index, value in enumerate(pmt_waveform_injected.get_pmt_pulse_times()):
-                if random_time == value:
-                    print('found pulse')
+                if random_time == value + 800:
                     num[OM_ID] += 1
-                    i_shape = pmt_waveform_injected.pmt_waveform_sweep_shape[800 - value]
-                    i_amp = pmt_waveform_injected.pmt_waveform_sweep_amp[800 - value]
-                    r_shape = pmt_waveform.pmt_waveform_sweep_shape[800 - value]
-                    r_amp = pmt_waveform.pmt_waveform_sweep_amp[800 - value]
+                    i_shape = pmt_waveform_injected.pmt_waveform_sweep_shape[value]
+                    i_amp = pmt_waveform_injected.pmt_waveform_sweep_amp[value]
+                    r_shape = pmt_waveform.pmt_waveform_sweep_shape[value]
+                    r_amp = pmt_waveform.pmt_waveform_sweep_amp[value]
+                    check = True
 
             # Store random numbers
             t_injected[OM_ID].append(random_time)
-            a_injected[OM_ID].append(random_amp)
+
+            if check:
+                a_injected[OM_ID].append(random_amp)
+                t_injected_success[OM_ID].append(random_time)
+                mf_a_injected[OM_ID].append(i_amp)
+                mf_s_injected[OM_ID].append(i_shape)
+            else:
+                a_injected_failure[OM_ID].append(random_amp)
+                t_injected_failure[OM_ID].append(random_time)
 
             injected_array[OM_ID][0].append(i_shape)
             injected_array[OM_ID][1].append(i_amp)
@@ -162,7 +181,7 @@ def main():
 
             enum[OM_ID] += 1
         iterator += 1
-        if iterator == 1000:
+        if iterator == num_events:
             break
         #bar.update(iterator)
 
@@ -173,7 +192,94 @@ def main():
     for i in range(2):
         plt.plot(t_injected[i], injected_array[i][0], 'r.', label="injected")
         plt.plot(t_injected[i], random_array[i][0], 'g.', label="random")
-        plt.plot(t_injected[i], injected_array[i][0], 'b.', label="best")
+        plt.plot(t_injected[i], best_array[i][0], 'b.', label="best")
+        plt.xlabel("time /ns")
+        plt.ylabel("shape index")
+        plt.title("Channel: {} events: {}".format(i, num_events))
+        plt.grid(True)
+        plt.legend()
+        plt.axhline(y=shape_cut, color='k', linestyle='-')
+        plt.savefig("/Users/willquinn/Desktop/pmt_sim_results/shape_index_vs_time_ch{}".format(i))
+        plt.show()
+
+        plt.hist(injected_array[i][0], bins=100, range=(0,1), alpha=0.5, label="injected")
+        plt.hist(random_array[i][0], bins=100, range=(0,1), alpha=0.5, label="random")
+        plt.hist(best_array[i][0], bins=100, range=(0,1), alpha=0.5, label="best")
+        plt.xlabel("shape index")
+        plt.ylabel("counts")
+        plt.title("Channel: {} events: {}".format(i, num_events))
+        plt.grid(True)
+        plt.yscale('log')
+        plt.legend()
+        plt.axvline(x=shape_cut, color='r', linestyle='-')
+        plt.savefig("/Users/willquinn/Desktop/pmt_sim_results/shape_index_vs_time_hist_ch{}".format(i))
+        plt.show()
+
+        plt.plot(t_injected_failure[i], a_injected_failure[i], 'r.', label="failures")
+        plt.plot(t_injected_success[i], a_injected[i], 'g.', label="success")
+        plt.xlabel("time /ns")
+        plt.title("Channel: {} events: {}".format(i, num_events))
+        plt.ylabel("amplitude index")
+        plt.grid(True)
+        plt.legend()
+        plt.savefig("/Users/willquinn/Desktop/pmt_sim_results/amplitude_success_vs_failures_ch{}".format(i))
+        plt.show()
+
+        plt.hist(a_injected_failure[i], bins=int(ran_range/2), range=(0, ran_range), alpha=0.5, label="failures")
+        plt.hist(a_injected[i], bins=int(ran_range/2), range=(0, ran_range), alpha=0.5, label="success")
+        plt.xlabel("amplitude index")
+        plt.title("Channel: {} events: {}".format(i, num_events))
+        plt.ylabel("counts")
+        plt.grid(True)
+        plt.legend()
+        plt.text(0, 0, 'Success percentage: {}%'.format(round(num[0]/enum[0] * 100), 4))
+        plt.savefig("/Users/willquinn/Desktop/pmt_sim_results/amplitude_success_vs_failures_hist_ch{}".format(i))
+        plt.show()
+
+        new_mf_a_y = []
+        new_mf_a_y_err = []
+        new_mf_a_x = np.histogram(a_injected[i], bins=int(ran_range/2), range=(0,ran_range))
+        x = []
+        for j in range(new_mf_a_x[0].size):
+
+            temp = []
+            for k in range(len(a_injected[i])):
+                if a_injected[i][k] == j * (ran_range / new_mf_a_x[0].size):
+                    temp.append(mf_a_injected[i][k])
+            x.append(j * (ran_range / new_mf_a_x[0].size))
+            new_mf_a_y.append(np.average(temp))
+            new_mf_a_y_err.append(np.std(temp))
+
+        plt.plot(a_injected[i], mf_a_injected[i], 'b.')
+        plt.errorbar(x, new_mf_a_y, yerr=new_mf_a_y_err, fmt='ro')
+        plt.xlabel("random amplitude injected /mV")
+        plt.title("Channel: {} events: {}".format(i, num_events))
+        plt.ylabel("amplitude index")
+        plt.axhline(y=amp_cut, color='g', linestyle='-')
+        plt.grid(True)
+        plt.savefig("/Users/willquinn/Desktop/pmt_sim_results/inj_vs_mf_amplitudes_ch{}".format(i))
+        plt.show()
+
+        new_mf_s_y = []
+        new_mf_s_y_err = []
+        new_mf_s_x = np.histogram(a_injected[i], bins=int(ran_range / 2), range=(0, ran_range))
+        x = []
+        for j in range(new_mf_s_x[0].size):
+            temp = []
+            for k in range(len(a_injected[i])):
+                if a_injected[i][k] == j * (ran_range / new_mf_a_x[0].size):
+                    temp.append(mf_s_injected[i][k])
+            x.append(j * (ran_range / new_mf_a_x[0].size))
+            new_mf_s_y.append(np.average(temp))
+            new_mf_s_y_err.append(np.std(temp))
+
+        plt.plot(a_injected[i], mf_s_injected[i], 'b.')
+        plt.errorbar(x, new_mf_s_y, yerr=new_mf_s_y_err, fmt='ro')
+        plt.xlabel("random amplitude injected /mV")
+        plt.title("Channel: {} events: {}".format(i, num_events))
+        plt.ylabel("shape index")
+        plt.grid(True)
+        plt.savefig("/Users/willquinn/Desktop/pmt_sim_results/inj_amp_vs_shape_ch{}".format(i))
         plt.show()
 
     output_file.Close()
